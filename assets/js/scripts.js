9a7063bbc5f25139fce8aed0615089cf9e27ce18
Breezy Fasano <breezyfasano@gmail.com>
2022-05-26 10:32:50 -0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,7 +7,7 @@ let changeQuote = (arr, element) => {
 
 window.addEventListener('load', function (event) {
     // Home page quotes
-    const quotes = ["fuck this job *goes to work*", "YOWAIMO", "time for a little treat", "i'm surprised there are people stupider than me", "wake up early? take 10 damage", "do it for serentia", "don't forget - you're a material gowrl"];
+    const quotes = ["fuck this job *goes to work*", "YOWAIMO", "time for a little treat", "i'm surprised there are people stupider than me", "wake up early? take 10 damage", "do it for serentia", "don't forget - you're a material gowrl", "anyone that hurts you can kick rocks", "night out with the girls *takes out Asuka, Rei, and Mari*", "when we have a lil coffee, life is daijoubu", "do it for claude", "time to put on the ol' 9 to 5 cat ears"];
     const text = document.querySelector('.quotes');
     changeQuote(quotes, text);
 });
